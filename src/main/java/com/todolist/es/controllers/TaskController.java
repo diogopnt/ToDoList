b7fb8b96a396ea4f5b025e1f632cfba6b24fc470
diogopnt <pintodiogo@ua.pt>
2024-10-26 10:47:05 +0100

    TOD-17 Create new task (backend)

--- src/main/java/com/todolist/es/controllers/TaskController.java
+++ src/main/java/com/todolist/es/controllers/TaskController.java
@@ -1,9 +1,19 @@
 package com.todolist.es.controllers;
 
 import org.springframework.web.bind.annotation.*;
+import com.todolist.es.models.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import com.todolist.es.services.TaskService;
+import org.springframework.stereotype.Controller;
 
 @RestController
 @RequestMapping("/api/v1/tasks")
 public class TaskController {
-
+    @Autowired
+    private TaskService taskService;
+    @PostMapping("/")
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+        return ResponseEntity.ok(taskService.createNewTask(task));
+    }
 }
--- src/main/java/com/todolist/es/services/TaskService.java
+++ src/main/java/com/todolist/es/services/TaskService.java
@@ -1,8 +1,16 @@
 package com.todolist.es.services;
 
+import com.todolist.es.models.Task;
+import com.todolist.es.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
 
+    public Task createNewTask(Task task) {
+        return taskRepository.save(task);
+    }
 }
